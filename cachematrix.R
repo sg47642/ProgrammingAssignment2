122137547d9431a26996ba3ccb062a00d0ffd93c
Sayan Ghosh <sg47642@gmail.com>
2015-03-21 15:25:46 -0500

    Added code for Matrix Inverse from Cache for R programming assignmnet 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## returns the inverse of a materix from cache
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
